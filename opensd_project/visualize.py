b09b9d00760f2cdf9f9864e290fb32bb3b005eef
MGA041 <marko.gantar@wartsila.com>
2023-06-28 14:44:25 +0200

    removed previous change, added test comment

--- opensd_project/visualize.py
+++ opensd_project/visualize.py
@@ -18,12 +18,12 @@ def show_reference_image(images, points=[], roi_size=None, bit_depth=12):
     Returns:
         fig: Matplotlib figure.
     """
+    # test comment
     fig, ax = plt.subplots()
     ax.imshow(images[0], cmap='gray', vmin=0, vmax=2**bit_depth)
 
     if points:
         ax.scatter(np.array(points)[:, 1], np.array(points)[:, 0], marker='.', color='r')
-        ax.set_title('Show reference image')
         
         if roi_size is not None:
             for point in np.array(points):
