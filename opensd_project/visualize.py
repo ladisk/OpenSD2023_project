b067ced74fa24a76edd437f26f8daed9ace55290
TengjiaoJiang <tengjiaojiang@outlook.com>
2023-06-28 14:30:08 +0200

    This is an example.

--- opensd_project/visualize.py
+++ opensd_project/visualize.py
@@ -23,6 +23,8 @@ def show_reference_image(images, points=[], roi_size=None, bit_depth=12):
 
     if points:
         ax.scatter(np.array(points)[:, 1], np.array(points)[:, 0], marker='.', color='r')
+        ax.set_xlabel("xx")
+        ax.set_ylabel("yy")
         
         if roi_size is not None:
             for point in np.array(points):
